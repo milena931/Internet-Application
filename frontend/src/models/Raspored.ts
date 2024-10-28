7239a4718863f917f473ef082be45cb7ac01924c
milena931 <milena.vuletic.03@gmail.com>
2024-10-28 15:00:13 +0100

    Delete frontend/src directory

--- frontend/src/models/Raspored.ts
+++
@@ -1,8 +0,0 @@
-export class Raspored{
-    tip_elementa: string = ""
-    x: number = 0
-    y: number = 0
-    poluprecnik: number = 0
-    brojLjudiZaStolom: number = 6
-    brojStola: number = 0
-}
